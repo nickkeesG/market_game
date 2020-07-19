4f2be393ae29aca45250058072f6c211f5f31654
Mr. Robot <email@email.com>
2020-07-18 18:54:28 -0700

    fixed a possible infinite loop

--- code/iterated_best_response.py
+++ code/iterated_best_response.py
@@ -6,7 +6,7 @@ def find_best_strategy(agent, current_price):
     best_utility = agent.get_expected_utility(best_strategy, current_price)
 
     step = 0.1
-    while abs(step) > 0.000000001:
+    while abs(step) > 0.000000001 and best_strategy < 1 and best_strategy > 0:
         best_strategy += step
         new_util = agent.get_expected_utility(best_strategy, current_price)
         if best_utility >= new_util:
@@ -27,7 +27,7 @@ def run_ibr(game, verbose = True):
         for idx in order:
             current_price = game.get_price()
             best_strategy = find_best_strategy(game.agents[idx], current_price)
-            if abs(best_strategy - game.agents[idx].strategy) > 0.00001:
+            if abs(best_strategy - game.agents[idx].strategy) > 0.0000001:
                 converged = False
                 if verbose:
                     print("Agent: ", idx, "\told_strat:", game.agents[idx].strategy, "\tnew_strat:" , best_strategy)
